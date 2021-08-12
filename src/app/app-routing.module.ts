99567bf9755695d0b7775a54db2eeb72de1f23d3
JosueOb <josuericardocando@hotmail.com>
2021-08-12 10:42:05 -0500

    Initialization of MSAL Angular v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [MsalGuard] },
   { path: 'login', component: LoginComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MsalInterceptor, MsalRedirectComponent } from '@azure/msal-angular';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/login/login.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MsalBroadcastService,
+  MsalGuard,
+  MsalModule,
+  MsalService,
+} from '@azure/msal-angular';
+import {
+  PublicClientApplication,
+  BrowserCacheLocation,
+  InteractionType,
+} from '@azure/msal-browser';
+
+import { AUTHORITY, CLIENT_ID, SCOPES } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
@@ -22,8 +38,44 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
+    MsalModule.forRoot(
+      new PublicClientApplication({
+        // MSAL Configuration
+        auth: {
+          clientId: CLIENT_ID,
+          authority: AUTHORITY,
+          //redirectUri: '/',
+        },
+        cache: {
+          cacheLocation: BrowserCacheLocation.LocalStorage,
+          storeAuthStateInCookie: true, // set to true for IE 11
+        },
+      }),
+      {
+        // MSAL Guard Configuration
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: SCOPES,
+        },
+        loginFailedRoute: '/login',
+      },
+      {
+        // MSAL Interceptor Configuration
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map([]),
+      }
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule {}
